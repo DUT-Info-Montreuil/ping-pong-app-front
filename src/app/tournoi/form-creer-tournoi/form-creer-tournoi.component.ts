97b2ce77aa7663a1d08a69ca0544415763493fc5
XiangLuc <xiangluc01@gmail.com>
2024-03-27 14:48:53 +0100

    mise en place d'un formulaire en 3 étapes

--- src/app/tournoi/form-creer-tournoi/form-creer-tournoi.component.ts
+++ src/app/tournoi/form-creer-tournoi/form-creer-tournoi.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { TournoiService } from '../tournoi.service';
 import { NewTournoi } from '../model/Tournoi';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-form-creer-tournoi',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './form-creer-tournoi.component.html',
   styleUrl: './form-creer-tournoi.component.css'
 })
 export class FormCreerTournoiComponent {
 
+  etape: number = 1;
+
   newTournoi: NewTournoi = {
-    format: '',
+    format: 'Simple',
     niveau: '',
     date: new Date(),
     duree: '',
@@ -23,18 +26,31 @@ export class FormCreerTournoiComponent {
 
   constructor(private tournoiService: TournoiService) {}
 
+  updateProgress(step: number) {
+    this.etape = step;
+  }
+
+  avancerEtape() {
+    this.etape++;
+  }
+
+  reculerEtape() {
+    this.etape--;
+  }
+
   addTournoi() {
     this.tournoiService.createTournoi(this.newTournoi).subscribe(
       (next) => {
         console.log(next);
         this.newTournoi = {
-          format: '',
+          format: 'Solo',
           niveau: '',
           date: new Date(),
           duree: '',
           lieu: '',
           status: false
         };
+        this.etape = 1;
       }
     )
   }
