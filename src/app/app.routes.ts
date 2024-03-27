4adecba026e5316ad8bb7ad1b3ed4730f94cbc58
XiangLuc <xiangluc01@gmail.com>
2024-03-27 11:16:33 +0100

    création d'une route pour accéder au composant du formulaire de création d'un tournoi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { JoueurListComponent } from './joueur/joueur-list/joueur-list.component';
 import { AddJoueurFormComponent } from './joueur/add-joueur-form/add-joueur-form.component';
 import { DetailsJoueurComponent } from './joueur/details-joueur/details-joueur.component';
+import { FormCreerTournoiComponent } from './tournoi/form-creer-tournoi/form-creer-tournoi.component';
 
 
 export const routes: Routes = [
     { path: 'liste_joueurs', component: JoueurListComponent },
     { path: 'form-joueur', component: AddJoueurFormComponent},
-    { path: 'joueur/:id', component: DetailsJoueurComponent}
+    { path: 'joueur/:id', component: DetailsJoueurComponent},
+    { path: 'form-tournoi', component: FormCreerTournoiComponent}
 ];
