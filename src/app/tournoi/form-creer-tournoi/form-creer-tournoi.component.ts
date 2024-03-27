1e14e4bf062081372897b95830aeb8c8a64b9f13
XiangLuc <xiangluc01@gmail.com>
2024-03-27 11:15:02 +0100

    création d'un composant pour la création de tournoi à travers un formulaire

---
+++ src/app/tournoi/form-creer-tournoi/form-creer-tournoi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-creer-tournoi',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-creer-tournoi.component.html',
+  styleUrl: './form-creer-tournoi.component.css'
+})
+export class FormCreerTournoiComponent {
+
+}
