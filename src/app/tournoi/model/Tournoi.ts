fe37c8019298876c3fe724b0655436e24adff7d7
XiangLuc <xiangluc01@gmail.com>
2024-04-01 18:40:12 +0200

    changement de l'interface Tournoi, ou on remplace la liste de joueur par une liste de match.

--- src/app/tournoi/model/Tournoi.ts
+++ src/app/tournoi/model/Tournoi.ts
@@ -1,18 +1,22 @@
+import { Match } from "../../match/model/Match"
+
 export interface Tournoi {
     _id: string,
     format: string,
     niveau: string,
-    date: Date,
+    date: string,
     duree: string,
     lieu: string,
+    matchs: Match[],
     status: boolean
 }
 
 export interface NewTournoi {
     format: string,
     niveau: string,
-    date: Date,
-    duree: string,
+    date: string,
+    duree: number,
     lieu: string,
+    matchs: Match[],
     status: boolean
 }
