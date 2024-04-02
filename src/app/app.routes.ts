e1b882bda85541a8910570a6e2df66188cde216f
XiangLuc <xiangluc01@gmail.com>
2024-04-02 09:55:20 +0200

    ajout d'un nouveau chemin pour les réponses de formulaire

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { JoueurListComponent } from './joueur/joueur-list/joueur-list.component'
 import { AddJoueurFormComponent } from './joueur/add-joueur-form/add-joueur-form.component';
 import { DetailsJoueurComponent } from './joueur/details-joueur/details-joueur.component';
 import { FormCreerTournoiComponent } from './tournoi/form-creer-tournoi/form-creer-tournoi.component';
+import { ResponseComponent } from './response/response/response.component';
 
 
 export const routes: Routes = [
     { path: 'liste_joueurs', component: JoueurListComponent },
     { path: 'form-joueur', component: AddJoueurFormComponent},
     { path: 'joueur/:id', component: DetailsJoueurComponent},
-    { path: 'form-tournoi', component: FormCreerTournoiComponent}
+    { path: 'form-tournoi', component: FormCreerTournoiComponent},
+    { path: 'success', component: ResponseComponent}
 ];
