141711252aaee4fcb8f9ea430e5e17af509fddd2
XiangLuc <xiangluc01@gmail.com>
2024-04-02 14:28:37 +0200

    ajout dans la nav de la liste des tournois

--- src/app/tournoi/list-tournois/list-tournois.component.ts
+++ src/app/tournoi/list-tournois/list-tournois.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TournoiService } from '../tournoi.service';
+import { Tournoi } from '../model/Tournoi';
 
 @Component({
   selector: 'app-list-tournois',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './list-tournois.component.html',
   styleUrl: './list-tournois.component.css'
 })
-export class ListTournoisComponent {
+export class ListTournoisComponent implements OnInit {
+
+  liste_tournoi: Tournoi[] = [];
+
+  constructor(private tournoiService: TournoiService) {}
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
